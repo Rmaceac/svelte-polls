35f3bc4ead7a001ab2f24a4abb9a41d1d3e8255d
Rmaceac <ryan.maceachern.tech@gmail.com>
2023-07-31 19:59:29 -0700

    create footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
 	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
 </script>
 
 <main>
 	<Header />
 	<h1>Hi Ryan!</h1>
+	<Footer />
 </main>
 
 <style>
