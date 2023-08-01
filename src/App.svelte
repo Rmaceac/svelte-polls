12739f01096e9a484690a6e20a29ed068f1528ca
Rmaceac <ryan.maceachern.tech@gmail.com>
2023-08-01 00:40:06 -0700

    create and update dabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script>
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import Tabs from "./components/shared/Tabs.svelte";
+
+	// tabs
+	let tabLabels = ['Current Polls', 'Add New Poll'];
+	let activeTab = ['Current Polls'];
 </script>
 
 <main>
 	<Header />
-	<h1>Hi Ryan!</h1>
+	<main>
+		<Tabs {tabLabels} {activeTab}/>
+	</main>
 	<Footer />
 </main>
 
---
+++ src/components/shared/Tabs.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let tabLabels;
+  export let activeTab;
+</script>
+
+<div class="tabs">
+  <ul>
+    {#each tabLabels as tab}
+      <li>
+        <div class:active={tab === activeTab}>{tab}</div>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .tabs {
+    margin-bottom: 40px;
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  li {
+    margin: 0 16px;
+    font-size: 18px;
+    color: #555;
+    cursor: pointer;
+  }
+  
+  .active {
+    color: #d91b42;
+    border-bottom: 2px solid #d91b42;
+    padding-bottom: 8px;
+  }
+</style>
